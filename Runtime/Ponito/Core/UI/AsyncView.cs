25d64351b7f220a19b0012d1e56239688f7d0927
小馬 <vvbdac58@gmail.com>
2024-03-28 18:02:05 +0800

    Adds ViewState

--- Runtime/Ponito/Core/UI/AsyncView.cs
+++ Runtime/Ponito/Core/UI/AsyncView.cs
@@ -17,10 +17,15 @@ namespace Ponito.Core.UI
         UniTask Show(object args);
         
         /// <summary>
-        ///     Smart manage your <see cref="AsyncView"/> to <see cref="Show"/>
+        ///     Smart manage your <see cref="AsyncView"/> to <see cref="Hide"/>
         /// </summary>
         /// <param name="args">any arguments you wish to pass into</param>
         /// <returns><see cref="UniTask"/> of itself</returns>
         UniTask Hide(object args);
+        
+        /// <summary>
+        ///     Gets the state of the <see cref="AsyncView"/>
+        /// </summary>
+        ViewState State { get; }
     }
 }
---
+++ Runtime/Ponito/Core/UI/ViewState.cs
@@ -0,0 +1,23 @@
+﻿namespace Ponito.Core.UI
+{
+    /// <summary>
+    ///     Common states of a <see cref="AsyncView"/>
+    /// </summary>
+    public enum ViewState
+    {
+        /// <summary>
+        ///     The <see cref="AsyncView"/> is on
+        /// </summary>
+        Showing,
+        
+        /// <summary>
+        ///     The <see cref="AsyncView"/> is in between on and off
+        /// </summary>
+        Transitioning,
+        
+        /// <summary>
+        ///     The <see cref="AsyncView"/> is off
+        /// </summary>
+        Hidden,
+    }
+}
