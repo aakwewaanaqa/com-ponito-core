9adbc7ef0a6212861f376f92d758efc7e21868a3
aakwewaanaqa <vvbdac58@gmail.com>
2024-05-26 20:48:19 +0800

    加上中文註解

--- Runtime/Ponito/Core/Asyncs/Interfaces/Movable.cs
+++ Runtime/Ponito/Core/Asyncs/Interfaces/Movable.cs
@@ -1,20 +1,58 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using Ponito.Core.Asyncs.Tasks;
 
 namespace Ponito.Core.Asyncs.Interfaces
 {
+    /// <summary>
+    ///     <see cref="MovableRunner"/> 中每個被等待者的核心功能的接口
+    /// </summary>
     public interface Movable : INotifyCompletion
     {
-        Exception Exception   { set; get; }
-        bool      IsCompleted { get; }
-        bool      MoveNext();
-        void      GetResult();
-        Movable   GetAwaiter();
+        /// <summary>
+        ///     任何在這個 <see cref="Movable"/> 中存在的 <see cref="Exception"/>
+        ///     當涉及到 <see cref="PoTask"/> 時，可能會有內部例外......
+        /// </summary>
+        Exception Exception { set; get; }
+
+        /// <summary>
+        ///     這個 <see cref="Movable"/> 是否已經完成移動？
+        ///     <see cref="PoTask"/> 會有已完成的內部 <see cref="Movable"/>。
+        /// </summary>
+        bool IsCompleted { get; }
+
+        /// <summary>
+        ///     把<see cref="Movable"/>往前推移的函式，通常為<see cref="MovableRunner"/>使用。
+        /// </summary>
+        /// <returns>
+        ///     <see cref="Movable"/>完成了沒？
+        /// </returns>
+        bool MoveNext();
+
+        /// <summary>
+        ///     取得結果！
+        /// </summary>
+        void GetResult();
+
+        /// <summary>
+        ///     取得等待者。
+        /// </summary>
+        /// <returns>等待者</returns>
+        Movable GetAwaiter();
     }
 
+    /// <inheritdoc />
     public interface Movable<out T> : Movable
     {
-        new T          GetResult();
+        /// <summary>
+        ///     取得結果！
+        /// </summary>
+        new T GetResult();
+
+        /// <summary>
+        ///     取得等待者。
+        /// </summary>
+        /// <returns>等待者</returns>
         new Movable<T> GetAwaiter();
     }
 }
